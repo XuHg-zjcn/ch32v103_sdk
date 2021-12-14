d4fcdc3d7ad554ad2dc1e93fabfb4ef64a7efeca
徐瑞骏 <1687701765@qq.com>
2021-12-14 18:19:32 +0800

    编译器标签注释

--- tools/conf.mk
+++ tools/conf.mk
@@ -5,27 +5,26 @@ CXX = "$(TOOL_CHAIN)g++"
 LINK = "$(TOOL_CHAIN)gcc"
 
 
-CCFLAGS := -Wall \
-    -march=rv32imac \
-    -mabi=ilp32 \
-    -msmall-data-limit=8 \
-    -mno-save-restore \
-    -Os \
-    -fmessage-length=0 \
-    -fsigned-char \
-    -ffunction-sections \
-    -fdata-sections \
-    -fno-common \
-    -Wunused \
-    -Wuninitialized \
-    -g \
-    -std=gnu99 \
-    -MMD -MP
+CCFLAGS += -march=rv32imac#      #架构
+CCFLAGS += -mabi=ilp32#          #
+CCFLAGS += -msmall-data-limit=8# #
+CCFLAGS += -mno-save-restore#    #
+CCFLAGS += -Os#                  #最小文件大小优化
+CCFLAGS += -fmessage-length=0#   #
+CCFLAGS += -fsigned-char#        #
+CCFLAGS += -ffunction-sections#  #
+CCFLAGS += -fdata-sections#      #
+CCFLAGS += -fno-common#          #
+CCFLAGS += -Wunused#             #
+CCFLAGS += -Wuninitialized#      #
+CCFLAGS += -g#                   #调试
+CCFLAGS += -std=gnu99#           #标准
+CCFLAGS += -nostartfiles
 
-CXXFLAGS := -Wall \
-    -std=gnu++11
+CXXFLAGS += -Wall#               #
+CXXFLAGS += -std=gnu++11#        #
 
-ASMFLAGS := -Wall
+ASMFLAGS += -Wall#               #
 
 
 CSRC ?= $(wildcard ${DIR}/*.c)
