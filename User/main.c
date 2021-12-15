7f74af8fa18d126f6fc3c5e92d3cc032fa2aa95a
徐瑞骏 <1687701765@qq.com>
2021-12-15 15:39:20 +0800

    PC13 LED测试失败

--- User/main.c
+++ User/main.c
@@ -6,16 +6,11 @@
 * Description        : Main program body.
 *******************************************************************************/
 
-/*
- *@Note
- 串口打印调试例程：
- USART1_Tx(PA9)。
- 本例程演示使用 USART1(PA9) 作打印调试口输出。
-
-*/
-
 #include "debug.h"
 
+
+#define LED_PORT GPIOC
+#define LED_PIN  GPIO_Pin_13
 
 /* Global typedef */
 
@@ -23,6 +18,15 @@
 
 /* Global Variable */
 
+void My_GPIO_Init()
+{
+	RCC_APB2PeriphResetCmd(RCC_APB2Periph_GPIOC, ENABLE);
+	GPIO_InitTypeDef gpio_init;
+	gpio_init.GPIO_Pin = LED_PIN;
+	gpio_init.GPIO_Speed = GPIO_Speed_50MHz;
+	gpio_init.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(LED_PORT, &gpio_init);
+}
 
 /*******************************************************************************
 * Function Name  : main
@@ -32,16 +36,21 @@
 *******************************************************************************/
 int main(void)
 {
+	SystemInit();
+	SystemCoreClockUpdate();
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
 	Delay_Init();
-	USART_Printf_Init(115200);
+	My_GPIO_Init();
 	//printf("SystemClk:%d\r\n",SystemCoreClock);
 
 	//printf("This is printf example\r\n");
 
 	while(1)
 	{
-
+		GPIO_SetBits(LED_PORT, LED_PIN);
+		Delay_Ms(100);
+		GPIO_ResetBits(LED_PORT, LED_PIN);
+		Delay_Ms(100);
 	}
 }
 
