0e50ab40cd233f1cf185a09df55585779b6fd367
徐瑞骏 <1687701765@qq.com>
2021-12-17 01:53:35 +0800

    应用许可证

--- tools/config.mk
+++ tools/config.mk
@@ -1,3 +1,21 @@
+###########################################################################
+#    工具链配置文件
+#    Copyright (C) 2021  Xu Ruijun
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+###########################################################################
+
 #MRS工具链路径，安装时需要修改成您自己的路径
 MRS_BASE = "/home/xrj/MRS/MounRiver Studio/toolchain"
 
