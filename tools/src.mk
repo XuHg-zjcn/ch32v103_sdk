b17a3c498701a48c7ea29b5582fbe63c6a771fd6
徐瑞骏 <1687701765@qq.com>
2022-07-16 23:17:20 +0800

    fix: 修复链接时警告 warning: cannot find entry symbol _start; defaulting to 0000000000000000

--- tools/src.mk
+++ tools/src.mk
@@ -1,8 +1,8 @@
-SDIR += $(TOP_DIR)/Core
-SDIR += $(TOP_DIR)/Startup
-SDIR += $(TOP_DIR)/Peripheral
-SDIR += $(TOP_DIR)/Debug
-SDIR += $(TOP_DIR)/User
-SDIR += $(TOP_DIR)/my_libs/exts/Src
-SDIR += $(TOP_DIR)/my_libs/ch32v10x/Src
-SDIR += $(TOP_DIR)/my_libs/common/others/Src
+SDIR += $(TOP_DIR)/Core/
+SDIR += $(TOP_DIR)/Startup/
+SDIR += $(TOP_DIR)/Peripheral/
+SDIR += $(TOP_DIR)/Debug/
+SDIR += $(TOP_DIR)/User/
+SDIR += $(TOP_DIR)/my_libs/exts/Src/
+SDIR += $(TOP_DIR)/my_libs/ch32v10x/Src/
+SDIR += $(TOP_DIR)/my_libs/common/others/Src/
